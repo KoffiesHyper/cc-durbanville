a771ba6f75467d03c248ee722a1f8b2108a2e09c
Christian Slier <koffieshyper@Christians-MacBook-Air.local>
2022-08-02 11:30:10 +0200

    update

--- src/pages/Giving/giving.js
+++ src/pages/Giving/giving.js
@@ -1,9 +1,44 @@
 import React from "react";
+import "./giving.css";
 
-export default function Giving({}){
-    return(
+export default function Giving({ }) {
+    return (
         <div>
-            Giving!
+            <div className="giving-cover-image">
+                <div className="welcome-message">
+                    <div className="hori-bar"></div>
+                    <h1>Giving</h1>
+                    <div className="hori-bar"></div>
+                </div>
+            </div>
+            <div className="giving-verses">
+                <div className="giving-verse">
+                    <h2>Luke 6:38</h2>
+                    <p>“Give, and it will be given to you: good measure, pressed down, shaken together, and running over will be put into your bosom. For with the same measure that you use, it will be measured back to you.”</p>
+                </div>
+                <div className="giving-verse">
+                    <h2>2 Corinthians 9:7</h2>
+                    <p>"So let each one give as he purposes in his heart, not grudgingly or of necessity; for God loves a cheerful giver." </p>
+                </div>
+                <div className="giving-verse">
+                    <h2>Matthew 6:3-4</h2>
+                    <p>"But when you do a charitable deed, do not let your left hand know what your right hand is doing, "that your charitable deed may be in secret; and your Father who sees in secret will Himself reward you openly."</p>
+                </div>
+            </div>
+            <div className="info-container">
+                <div className="more-giving-info">
+                    <h2>More about Finances</h2>
+                    <p>At Calvary Chapel we view giving as an act of worship. God loves a cheerful giver because this act exemplifies the condition of a surrendered heart.</p>
+                    <p>God also teaches that what is done in secret will be rewarded in the open. It is obvious that any church needs finances to function, but we choose not to make this the main thing, we do not use a collection plate and seldom mention money at our services. We trust worshippers who worship in Spirit and Truth to keep what is secret between the worshipper and God. In the same way the church board is trusted by the worshipper to handle such finance with Godly Stewardship. As an act of transparency, finance records therefor, are periodically made availabe for active church members to view on request. </p>
+                </div>
+                <div className="bank-details">
+                    <h2>Account Details</h2>
+                    <p><span style={{ fontFamily: 'custom3' }}>Bank: </span>Capitec</p>
+                    <p><span style={{ fontFamily: 'custom3' }}>Account Type: </span>Savings</p>
+                    <p><span style={{ fontFamily: 'custom3' }}>Account Number: </span>1676051714</p>
+                    <p><span style={{ fontFamily: 'custom3' }}>Branch Code: </span>470010</p>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/Team/team.js
+++ src/pages/Team/team.js
@@ -1,9 +1,24 @@
 import React from "react";
+import "./team.css";
 
 export default function Team({}){
     return(
         <div>
-            Ministry Team!
+            <div className="team-cover-image">
+                <div className="welcome-message">
+                    <div className="hori-bar"></div>
+                    <h1>Ministry Team</h1>
+                    <div className="hori-bar"></div>
+                </div>
+            </div>
+            <div className="team-info">
+                <img src="https://lh3.googleusercontent.com/f993wULJXGDR5QoVhmUbanrFqh6sWO-3IohB3xSCExCNY5b4fhB-sxzWJ7JEfn_CtTCTK8A811L-EybYDv_tp8ssAvB3UtpWeIjg0vwKQUrAWCTp46UcfLS9f-Bm_1v5OA=w1280" />
+                <div className="team-info-text">
+                    <p>Pastor Theunis Möller (Boet) has been with Calvary Chapel for 17 years.</p>
+                    <p>He began the Calvary Chapel Durbanville plant in Dec 2018, along with his wife Ruth, and two daughters - Hannah and Penny.</p>
+                    <p>He has a passion and a calling to teach the Word of God faithfully, and to make disciples for Jesus Christ in Durbanville, and beyond.</p>
+                </div>
+            </div>
         </div>
     );
 }
